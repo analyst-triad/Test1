2dbf424a8a69b5a28a2f3c405f867241790038d7
analyst-triad <saadfarooq404@yahoo.com>
2023-09-22 10:45:24 +0500

    added new function and testt

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from main import add
+from main import add,sub
 
 def test_add():
     assert 0 == add(0,0)
     assert -1 == add(1,-2)
+    
+def test_sub():
+    assert 0 == sub(1,1)
+    assert 5 == sub(6,1)
